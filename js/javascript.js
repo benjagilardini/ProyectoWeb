7e132479242c1de57239f77486e9a0a815c286b3
benjagilardini <benja.gilardini@gmail.com>
2017-11-28 10:56:36 -0300

    mejorafinal

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,23 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 var database = firebase.database();
 
+$(document).ready(function (){
+    Actualizar();
+});
+
+
 function googleSignin() {
     firebase.auth()
-    .signInWithPopup(provider).then(function(result) {
+        .signInWithPopup(provider).then(function(result) {
         var token = result.credential.accessToken;
         var user = result.user;
-      
+
         console.log(token)
         console.log(user)
     }).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        
+
         console.log(error.code)
         console.log(error.message)
     });
@@ -21,7 +26,7 @@ function googleSignin() {
 function googleSignout() {
     firebase.auth().signOut()
 
-    .then(function() {
+        .then(function() {
         console.log('Signout Succesfull')
     }, function(error) {
         console.log('Signout Failed')  
@@ -30,22 +35,60 @@ function googleSignout() {
 
 function publicar(){
 
+    var key = firebase.database().ref().push().key;
     textorespuesta = document.getElementById('gettexto').value;
+
     var data = {
-        texto: textorespuesta
+        key: key,
+        texto: textorespuesta,
+        resp: {test: "test"}
     }
-    var publicaciones = database.ref('publicaciones');
-    database.ref('publicaciones').push(data);
+
+    var updates = {};
+    updates["/publicaciones/" + key] = data;
+
+    firebase.database().ref().update(updates);
+
     window.scrollBy(0,document.body.clientHeight*5);
     document.getElementById('gettexto').value = "";
 }
+
+//Este metodo actualiza las entradas al foro
+
 function Actualizar(){
-    var refpublicacion = firebase.database().ref("/publicaciones/");
-    refpublicacion.on("child_added",snapshot =>{
-        $('body').append('<div class="divBody">' + snapshot.val().texto + '<button class="btn waves-effect waves-light responder" onclick="responder();" type="submit" name="action">Responder <i class="material-icons right">send</i></button></p></div>');
-   });
-  
-}
- $(document).ready(function (){
-        Actualizar();
+    var refpublicacion = firebase.database().ref("publicaciones/");
+
+    refpublicacion.on("value", snapshot => {
+
+        $("#comments").empty();
+
+        snapshot.forEach(snaps => {
+
+            $('#comments').append('<div id="' + snaps.val().key + '" class="divBody">' + snaps.val().texto + '</div>');
+
+        });
+
     });
+
+}
+
+
+function responder(key){
+    
+    console.log(key);
+
+    contestar = document.getElementById('input_' + key).value;
+
+    var key_novedades = firebase.database().ref().push().key;
+
+    var data = {
+        key: key_novedades,
+        texto: contestar
+    }
+
+    var updates = {};
+    updates["publicaciones/" + key + "/resp/" + key_novedades] = data;
+
+    firebase.database().ref().update(updates);
+
+}
